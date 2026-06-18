789a1418221facdd32c45e1e1c5434163907454d
Abhihack12 <rajnarayanganjpandey@gmail.com>
2026-06-18 21:33:19 +0530

    Setup.sh
    Ye tool ko setup karega first run this

---
+++ setup.sh
@@ -0,0 +1,9 @@
+apt update
+apt upgrade
+pkg update -y
+pkg upgrade -y
+pkg install proot -y
+pkg install tar -y
+chmod +x DistroBOXv1.1.sh
+chmod +x DistroBOXv1.2.sh
+echo -e "\e[1;32m [*]setup completed\e[92m"
